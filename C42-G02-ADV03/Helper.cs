2c2be99fc24b34497e186c784936b7c8116a0747
mali84211 <mali84211@gmail.com>
2024-08-05 20:03:00 +0300

    Delegate EX

---
+++ C42-G02-ADV03/Helper.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace C42_G02_ADV03
+{
+    internal static class Helper
+    {
+        //public static List<int> GetOddnumbers(List<int> Numbers) {
+        //   List<int> Result = new List<int>();
+        //    if (Numbers is not null)
+        //    {
+        //        for (int i = 0; i < Numbers.Count; i++)
+        //        {
+        //            if (Numbers[i] %2==1)
+        //          Result.Add(Numbers[i]);
+        //        }
+        //        return Result;
+        //    }
+        //}
+        //public static List<int> GetEvennumbers(List<int> Numbers)
+        //{
+        //    List<int> Result = new List<int>();
+        //    if (Numbers is not null)
+        //    {
+        //        for (int i = 0; i < Numbers.Count; i++)
+        //        {
+        //            if (Numbers[i] % 2 == 1)
+        //                Result.Add(Numbers[i]);
+        //        }
+        //        return Result;
+        //    }
+        //}
+        //public static List<int> GetNumbersDivisibleBy7(List<int> Numbers)
+        //{
+        //    List<int> Result = new List<int>();
+        //    if (Numbers is not null)
+        //    {
+        //        for (int i = 0; i < Numbers.Count; i++)
+        //        {
+        //            if (Numbers[i] % 7 == 1)
+        //                Result.Add(Numbers[i]);
+        //        }
+        //        return Result;
+        //    }
+        //}
+    }
+}
--- C42-G02-ADV03/Program.cs
+++ C42-G02-ADV03/Program.cs
@@ -4,7 +4,41 @@
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            #region V1
+           // List<int> Numbers = Enumerable.Range(1,100).ToList();
+           //List<int>Oddnumbers= Helper.GetOddnumbers(Numbers);
+
+           // Console.WriteLine("ODD Numbers");
+           // foreach (int number in Oddnumbers)
+           // {
+           //     Console.WriteLine($"{number}");
+           // }
+
+           // List<int> Evennumbers = Helper.GetEvennumbers(Numbers);
+
+           // Console.WriteLine("\nEven Numbers");
+           // foreach (int number in Evennumbers)
+           // {
+           //     Console.WriteLine($"{number}");
+           // }
+
+           // List<int> GetNumbersDivisibleBy7 = Helper.GetNumbersDivisibleBy7(Numbers);
+
+           // Console.WriteLine("\nGetNumbersDivisibleBy7");
+           // foreach (int number in GetNumbersDivisibleBy7)
+           // {
+           //     Console.WriteLine($"{number}");
+           // }
+            #endregion
+
+            #region
+            #endregion
+
+            #region
+            #endregion
+
+            #region
+            #endregion
         }
     }
 }
